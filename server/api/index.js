0bb9bd6ac2ba87d49678dc0fdd276dbab1900dce
MiniPekka <chaoye917@gmail.com>
2017-05-31 23:47:14 -0700

    Remove redundant code

--- server/api/index.js
+++ server/api/index.js
@@ -16,51 +16,52 @@ const port = process.env.PORT || 3001;
 
 // Add headers
 app.use((req, res, next) => {
-    // http://stackoverflow.com/questions/24897801/enable-access-control-allow-origin-for-multiple-domains-in-nodejs
-    // Website you wish to allow to connect
-    var allowedOrigins = [
-      'http://127.0.0.1:3000',
-      'http://localhost:3000',
-      'http://127.0.0.1:8080',
-      'http://localhost:8080',
-      'https://redux-music-test.herokuapp.com',
-      'https://redux-music.herokuapp.com'
-    ];
-    var origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) > -1) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+  // http://stackoverflow.com/questions/24897801/enable-access-control-allow-origin-for-multiple-domains-in-nodejs
+  // Website you wish to allow to connect
+  var allowedOrigins = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://127.0.0.1:8080',
+    'http://localhost:8080',
+    'https://redux-music-test.herokuapp.com',
+    'https://redux-music.herokuapp.com',
+  ];
+  var origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
 
 // Simple logger
 function logger(req, res, next) {
-    console.log(new Date(), req.method, req.url);
-    next();
+  console.log(new Date(), req.method, req.url);
+  next();
 }
 
-const welcomeMessage = 'Welcome to SoundCloud API server! For more details go to: https://github.com/MiniPekka/redux-music-api';
+const welcomeMessage =
+  'Welcome to SoundCloud API server! For more details go to: https://github.com/MiniPekka/redux-music-api';
 
-app.get('/', function(req, res){
-    res.send(welcomeMessage);
+app.get('/', function(req, res) {
+  res.send(welcomeMessage);
 });
 
 app.use(logger);
 app.use('/', v2);
 
 app.listen(port, () => {
-    console.log(`API Server Started at:${port}`);
-    console.log(`API server is running in ${process.env.NODE_ENV} mode`);
+  console.log(`API Server Started at:${port}`);
+  console.log(`API server is running in ${process.env.NODE_ENV} mode`);
 });
